b5ab6bb2e47769fbd9a4e481b717edb87337d96f
ZverLightYear <zver.light.year@gmail.com>
2020-08-15 13:32:34 +0300

    Задокументировал изменения

--- core/saver_manager.py
+++ core/saver_manager.py
@@ -1,18 +1,30 @@
 from __future__ import annotations
-from abc import ABC
 
 from core.weather_saver import Weather, WeatherSaver
-from core.weather_provider import WeatherProvider
 from core.weather_savers.json_saver import JsonWeatherSaver
 from core.weather_savers.xml_saver import XmlWeatherSaver
 
 
-class WeatherSaverManager(ABC):
+class WeatherSaverManager:
+    """
+    Реализация мэнеджера погодных сэйверов.
+    """
+
     def __init__(self, config):
+        """
+        Инициализация менеджэра погодных сэйверов.
+        :param dict config: Конфигурация погодных сэйверов.
+        """
         self._savers = {"json": lambda: JsonWeatherSaver(config["json_format"]),
                         "xml": lambda: XmlWeatherSaver(config["xml_format"])}
 
     def save(self, weather: Weather, file_path, saver_type) -> Weather:
+        """
+        Сохранение информации о погоде.
+        :param Weather weather: Погода для сохранения.
+        :param str file_path: Путь сохранения файла.
+        :param str saver_type: тип используемого сэйвера.
+        """
         saver: WeatherSaver = self._savers[saver_type]()
         return saver.save(weather, file_path)
 
